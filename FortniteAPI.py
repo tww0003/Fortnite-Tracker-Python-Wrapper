1aab3b0bce09f85079a4122646d5bec8a3d30b3f
Tyler Williamson <tylerwil@gmail.com>
2018-05-24 09:32:46 -0500

    Changed coding style from PEP-8 to Black

--- FortniteAPI.py
+++ FortniteAPI.py
@@ -5,21 +5,21 @@ api_key = None
 
 
 class FortnitePlaylist(Enum):
-    SOLO          = 'p2'
-    DUO           = 'p10'
-    SQUAD         = 'p9'
-    CURRENT_SOLO  = 'curr_p2'
-    CURRENT_DUO   = 'curr_p10'
-    CURRENT_SQUAD = 'curr_p9'
+    SOLO = "p2"
+    DUO = "p10"
+    SQUAD = "p9"
+    CURRENT_SOLO = "curr_p2"
+    CURRENT_DUO = "curr_p10"
+    CURRENT_SQUAD = "curr_p9"
 
 
 class FortniteAPI:
-    base_url = 'https://api.fortnitetracker.com/v1/profile/'
+    base_url = "https://api.fortnitetracker.com/v1/profile/"
 
     def __init__(self, platform, user):
         self.user = user
         self.platform = platform
-        self.url = ''
+        self.url = ""
         self.update_url()
         self.json_data = None
         if api_key is not None:
@@ -34,118 +34,120 @@ class FortniteAPI:
         self.update_url()
 
     def update_url(self):
-        self.url = self.base_url + self.platform + '/' + self.user
+        self.url = self.base_url + self.platform + "/" + self.user
         if api_key is not None:
             self.make_request()
 
     def get_score(self, playlist):
         try:
-            return_data = self.json_data['stats'][playlist.value]['top10']['value']
+            return_data = self.json_data["stats"][playlist.value]["top10"]["value"]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
         return return_data
 
     def get_top_twelve(self, playlist):
         try:
-            return_data = self.json_data['stats'][playlist.value]['top12']['value']
+            return_data = self.json_data["stats"][playlist.value]["top12"]["value"]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
 
         return return_data
 
     def get_top_twenty_five(self, playlist):
         try:
-            return_data = self.json_data['stats'][playlist.value]['top25']['value']
+            return_data = self.json_data["stats"][playlist.value]["top25"]["value"]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
 
         return return_data
 
     def get_kd(self, playlist):
         try:
-            return_data = self.json_data['stats'][playlist.value]['kd']['value']
+            return_data = self.json_data["stats"][playlist.value]["kd"]["value"]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
 
         return return_data
 
     def get_matches(self, playlist):
         try:
-            return_data = self.json_data['stats'][playlist.value]['matches']['value']
+            return_data = self.json_data["stats"][playlist.value]["matches"]["value"]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
 
         return return_data
 
     def get_kills(self, playlist):
         try:
-            return_data = self.json_data['stats'][playlist.value]['kills']['value']
+            return_data = self.json_data["stats"][playlist.value]["kills"]["value"]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
 
         return return_data
 
     def get_kpg(self, playlist):
         try:
-            return_data = self.json_data['stats'][playlist.value]['kpg']['value']
+            return_data = self.json_data["stats"][playlist.value]["kpg"]["value"]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
 
         return return_data
 
     def get_score_per_match(self, playlist):
         try:
-            return_data = self.json_data['stats'][playlist.value]['scorePerMatch']['value']
+            return_data = self.json_data["stats"][playlist.value]["scorePerMatch"][
+                "value"
+            ]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
 
         return return_data
 
     def get_lifetime_score(self):
         try:
-            return_data = self.json_data['lifeTimeStats'][6]['value']
+            return_data = self.json_data["lifeTimeStats"][6]["value"]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
 
         return return_data
 
     def get_lifetime_matches_played(self):
         try:
-            return_data = self.json_data['lifeTimeStats'][7]['value']
+            return_data = self.json_data["lifeTimeStats"][7]["value"]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
 
         return return_data
 
     def get_lifetime_wins(self):
         try:
-            return_data = self.json_data['lifeTimeStats'][8]['value']
+            return_data = self.json_data["lifeTimeStats"][8]["value"]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
 
         return return_data
 
     def get_lifetime_win_percentage(self):
         try:
-            return_data = self.json_data['lifeTimeStats'][9]['value']
+            return_data = self.json_data["lifeTimeStats"][9]["value"]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
 
         return return_data
 
     def get_lifetime_kills(self):
         try:
-            return_data = self.json_data['lifeTimeStats'][10]['value']
+            return_data = self.json_data["lifeTimeStats"][10]["value"]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
 
         return return_data
 
     def get_lifetime_kd(self):
         try:
-            return_data = self.json_data['lifeTimeStats'][11]['value']
+            return_data = self.json_data["lifeTimeStats"][11]["value"]
         except KeyError:
-            return_data = '0'
+            return_data = "0"
 
         return return_data
 
@@ -155,7 +157,7 @@ class FortniteAPI:
         self.make_request()
 
     def make_request(self):
-        header = {'TRN-Api-Key': api_key}
+        header = {"TRN-Api-Key": api_key}
         req = requests.get(self.url, headers=header)
         req.json()
         self.json_data = req.json()

81d5dd22092a2971c02932b7a6b574c394c1530d
Tyler Williamson <tylerwil@gmail.com>
2018-09-04 11:26:09 -0500

    Added project to pypi as FortniteTrackerAPI

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
 from FortniteAPI import FortniteAPI
 
 FortniteAPI.api_key = 'fortnite_api_key'
-api = FortniteAPI('xbl', 'tylerw330')
+tyler = FortniteAPI('xbl', 'tylerw330')
 
-print('Solo kills per game: ' + str(api.stats.CURRENT_SOLO_KPG))
-print('Duo kills per game: ' + str(api.stats.CURRENT_DUO_KPG))
-print('Squad kills per game: ' + str(api.stats.CURRENT_SQUAD_KPG))
+print('Solo kills per game: ' + str(tyler.stats.CURRENT_SOLO_KPG))
+print('Duo kills per game: ' + str(tyler.stats.CURRENT_DUO_KPG))
+print('Squad kills per game: ' + str(tyler.stats.CURRENT_SQUAD_KPG))
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='FortniteTrackerAPI',
+    version='0.0.1',
+    author='Tyler Williamson',
+    author_email='tylerwil@gmail.com',
+    url='https://github.com/tww0003/Fortnite-Tracker-Python-Wrapper',
+    description='A simple package to grab stats via Fortnite Tracker and place them into an easy to use stats object',
+    long_description='A simple package to grab stats via Fortnite '
+                     'Tracker and place them into an easy to use stats object',
+    packages=['FortniteAPI'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
